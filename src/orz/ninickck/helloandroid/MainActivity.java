cb5307aea40488e747d68185c103e6807103bef6
Nick Wang <ninickck@gmail.com>
2012-11-23 17:38:28 +0800

    ha

--- src/orz/ninickck/helloandroid/MainActivity.java
+++ src/orz/ninickck/helloandroid/MainActivity.java
@@ -1,12 +1,17 @@
 package orz.ninickck.helloandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    @Override
+    public static final String EXTRA_MESSAGE = null;
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -18,5 +23,12 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
+   
+    public void sendMsg(View view){
+    	Intent intent = new Intent(this, ShowMsgActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.eT01);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
