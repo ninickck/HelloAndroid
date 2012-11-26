87c93ada15e21193a2611054ae1b60e4c4b95162
Nick Wang <ninickck@gmail.com>
2012-11-26 16:28:30 +0800

    add weibo sdk

--- gen/orz/ninickck/helloandroid/R.java
+++ gen/orz/ninickck/helloandroid/R.java
@@ -10,35 +10,47 @@ package orz.ninickck.helloandroid;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int dialog_title_blue=0x7f040002;
+        public static final int text_num_gray=0x7f040001;
+        public static final int transparent=0x7f040000;
+    }
+    public static final class dimen {
+        public static final int weibosdk_dialog_bottom_margin=0x7f050003;
+        public static final int weibosdk_dialog_left_margin=0x7f050000;
+        public static final int weibosdk_dialog_right_margin=0x7f050002;
+        public static final int weibosdk_dialog_top_margin=0x7f050001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int weibosdk_dialog_bg=0x7f020001;
     }
     public static final class id {
-        public static final int btn_send=0x7f070001;
-        public static final int btn_showMsg=0x7f070002;
-        public static final int eT01=0x7f070000;
-        public static final int menu_help=0x7f070005;
-        public static final int menu_settings=0x7f070004;
-        public static final int tV01=0x7f070003;
+        public static final int btn_send=0x7f090001;
+        public static final int btn_showMsg=0x7f090002;
+        public static final int eT01=0x7f090000;
+        public static final int menu_help=0x7f090005;
+        public static final int menu_settings=0x7f090004;
+        public static final int tV01=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_show_msg=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
-        public static final int activity_show_msg=0x7f060001;
+        public static final int activity_main=0x7f080000;
+        public static final int activity_show_msg=0x7f080001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int btn_send=0x7f040005;
-        public static final int edit_hint=0x7f040004;
-        public static final int hello=0x7f040001;
-        public static final int hello_world=0x7f040006;
-        public static final int menu_help=0x7f040003;
-        public static final int menu_settings=0x7f040002;
-        public static final int show_message=0x7f040008;
-        public static final int title_activity_show_msg=0x7f040007;
+        public static final int app_name=0x7f060000;
+        public static final int btn_send=0x7f060005;
+        public static final int edit_hint=0x7f060004;
+        public static final int hello=0x7f060001;
+        public static final int hello_world=0x7f060006;
+        public static final int menu_help=0x7f060003;
+        public static final int menu_settings=0x7f060002;
+        public static final int show_message=0x7f060008;
+        public static final int title_activity_show_msg=0x7f060007;
     }
     public static final class style {
         /** 
@@ -62,10 +74,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f070001;
     }
 }
